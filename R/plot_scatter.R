0c4bdc40fe416fe9a8064ffd017667096da0fcc1
zhijingjing1 <74650717+zhijingjing1@users.noreply.github.com>
2021-03-12 20:41:20 +0800

    Update and rename plot_bar.R to plot_scatter.R

--- R/plot_scatter.R
+++ R/plot_scatter.R
@@ -20,19 +20,3 @@
 scatter_plot <- function(data, x, y, color="green", title="Scatter plot", plot_width = 300, plot_height = 300){
 }
 
-
-#' Bind two factors
-#'
-#' Create a new factor from two existing factors, where the new factor's levels
-#' are the union of the levels of the input factors.
-#'
-#' @param a factor
-#' @param b factor
-#'
-#' @return factor
-#' @export
-#' @examples
-#' plot_bar(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
-plot_bar <- function(a, b) {
-  factor(c(as.character(a), as.character(b)))
-}
