d900aa312a5a832b554d06254feba09fa06ce924
Fei Chang <fc1271@nyu.edu>
2021-03-13 19:07:53 +0800

    Complete code for plot histogram

--- R/plot_hist.R
+++ R/plot_hist.R
@@ -11,7 +11,46 @@
 #' @export
 #'
 #' @examples
-#' plot_hist(mtcars, exclude = c(""))
-plot_hist <- function(input_df, density = FALSE, exclude = c(""), title = "Histogram"){
+#' plot_hist(mtcars, exclude = c())
+plot_hist <- function(input_df, density = FALSE, exclude = c(NA), title = ""){
+  #Input Check
+  if(!is.data.frame(input_df)){
+    stop("The 'input_df' should be a dataframe. Please check.")
+  }
+  if(!is.logical(density)){
+    stop("The 'density' should be a boolean. Please check")
+  }
+  if(!is.vector(exclude)){
+    stop("The 'exclude' should be a vector. Please check")
+  }
+  if(!is.character(title)){
+    stop("The 'title' should be characters. Please check")
+  }
+  if((!is.na(exclude)) & (!(exclude %in% colnames(input_df)))){
+    stop("Excluding columns which are not present in the input dataframe. Please check.")
+  }
+  
+  if(!is.na(exclude)){
+    numeric_df<- input_df %>% select(-all_of(exclude)) %>% select_if(is.numeric)
+  }else{
+    numeric_df<- input_df %>% select_if(is.numeric)
+  }
+  
+  if(length(numeric_df) == 0){
+    warning("No column selected. An empty vector will be returned.")
+  }
+  melt_df <- melt(numeric_df)
+  if(density){
+    plot <- ggplot(melt_df, aes(x = value))+
+      facet_wrap(~variable,scales = "free_x") + 
+      geom_density() +
+      ggtitle(title)
+  }else{
+    plot <- ggplot(melt_df, aes(x = value))+
+      facet_wrap(~variable,scales = "free_x") + 
+      geom_histogram() +
+      ggtitle(title)
+  }
+  return(plot)
   
 }
